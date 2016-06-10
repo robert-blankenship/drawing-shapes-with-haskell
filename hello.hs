b76311d3dbd39ce48d33ef1a6236e195a885f047
Robert Blankenship <rablanken13@gmail.com>
2016-06-10 05:32:40 +0000

    YES! Transcribing a regular polygon within another regular polygon finally works. :).

--- hello.hs
+++ hello.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Graphics.Blank 
+import Data.List
 import Debug.Trace
 
 main :: IO ()
@@ -10,6 +11,15 @@ go :: DeviceContext -> IO ()
 go context = send context $ do
     globalAlpha 0.5
     drawPolkaDots (width context) (height context)
+
+reducedAngle :: Double -> Double
+reducedAngle angle = if angle > (2 * pi) then reducedAngle (angle - 2*pi) else angle
+
+reducedAngles :: [Double] -> [Double]
+reducedAngles angles = map reducedAngle angles
+
+reorderedAngles :: [Double] -> [Double]
+reorderedAngles angles = sort (reducedAngles angles)
 
 drawPolkaDots :: Double -> Double -> Canvas ()
 drawPolkaDots sceneWidth sceneHeight = do
@@ -37,55 +47,73 @@ drawPolkaDots sceneWidth sceneHeight = do
                 else fillRect(x - rX, y - rX, 2 * rX, 2 * rX)
 
     let drawPolygon = \points -> do
-            moveTo(points !! 0)
-            foldr addLine (moveTo(points !! 0)) points
-                where addLine (x, y) lines = do
-                        lineTo(x, y)
-                        lines
+            moveTo $ head points
+
+            let addLine (x, y) lines = do
+                    lineTo(x, y)
+                    lines
+
+            foldr addLine (lineTo $ head points) points
+            stroke()
+
+    let drawRegularPolygon = \(xCenter, yCenter) l n angle -> do
+            let points = [ (x i, y i)  | i <- [0..(n-1)]]
+                    where
+                        angle' i = angle + (i * 2*pi/n)
+                        x i = xCenter + l * sin(angle' i)
+                        y i = yCenter + l * cos(angle' i)
+
+            drawPolygon points
     
     let drawTriangle = \(x1, y1) (x2, y2) (x3, y3) -> do
-        drawPolygon [(x1, y1), (x2, y2), (x3, y3)]
-        stroke()
+            drawPolygon [(x1, y1), (x2, y2), (x3, y3)]
 
     -- Starting with some outer square defined by x, y, r, 
     -- draw an inscribed triangle pointing at angle (where 0 degrees
     -- is 'up')
-    let reducedAngle angle = if angle > (2 * pi) then reducedAngle (angle - 2*pi) else angle
+    let polygonAngles n = [(i * 2*pi/n) | i <- [0..(n - 1)]]
 
-    let polygonAngles n = [(i * 2*pi/n) | i <- [0..n]]
+    let getInscribedPolygonLength outerDiagonalLength outerAngles innerAngles = trace (show possibleHeights) $ minimum possibleHeights
+            where
+                getHeight angle' =
+                     h_m / cos (abs (theta - theta_m))
+                     where
+                        majorAngle = if length (filter (angle' <=) outerAngles) > 0
+                                    then head $ filter (angle' <=) outerAngles
+                                    else head outerAngles + 2*pi
+                        minorAngle = if length (filter (angle' >=) outerAngles) > 0
+                                    then last $ filter (angle' >=) outerAngles
+                                    else last outerAngles - 2*pi
+                        theta = angle' - minorAngle
+                        theta_m = (majorAngle - minorAngle) / 2
+                        h_m = outerDiagonalLength * (cos theta_m)
 
-    let regularPolygonAligned n angle = \x y s1 s2 -> do
-            let s = (if s1 < s2 then s1 else s2) 
-            let h' = minimum possibleHeights
-                    where
-                        getHeight angle'
-                            | reducedAngle angle' < (pi/8) = abs (s / cos(angle'))
-                            | reducedAngle angle' < (3*pi/4) = abs (s / sin(angle'))
-                            | reducedAngle angle' < (5*pi/4) = abs (s / cos(angle'))
-                            | reducedAngle angle' < (7*pi/4) = abs (s / sin(angle'))
-                            | reducedAngle angle' <= (2*pi) = abs (s / cos(angle'))
-    
-                        angles = map (\angle' -> angle + angle') (polygonAngles n)
-                        possibleHeights = map getHeight angles
-    
-            let point angle' = (x + (h' * cos(angle + angle')), y + (h' * sin(angle + angle')))
-            let points = map (\a -> point a) (polygonAngles n)
-    
-            drawPolygon points
-            stroke()
-            strokeRect(x - s, y - s, 2*s, 2*s)
+                possibleHeights = map getHeight (reducedAngles innerAngles)
 
-    -- TODO: This shouldn't draw the first angle twice.
-    let regularPolygonAlignedMultiple n angle iterations = \x y s1 s2 -> do
-            let initialPolygon = (regularPolygonAligned n angle x y s1 s2)
-            let accumulatePolygons angle' accumulator = do
-                    regularPolygonAligned n angle' x y s1 s2
+    let inscribedRegularPolygonAligned (outerSidesCount, outerAngleStart) (innerSidesCount, innerAngleStart) = \x y s1 s2 -> do
+            let gridSideLength = (if s1 < s2 then s1 else s2)
+            let gridAngles = reorderedAngles (map (+ pi/4) (polygonAngles 4))
+            let gridDiagonalLength = gridSideLength * 1.414
+
+            let outerAngles = reorderedAngles (map (+ outerAngleStart) (polygonAngles outerSidesCount))
+            let outerDiagonalLength = getInscribedPolygonLength gridDiagonalLength gridAngles outerAngles
+
+            let innerAngles = reorderedAngles (map (+ innerAngleStart) (polygonAngles innerSidesCount))
+            let innerDiagonalLength = getInscribedPolygonLength outerDiagonalLength outerAngles innerAngles
+
+            drawRegularPolygon (x,y) innerDiagonalLength innerSidesCount (head innerAngles)
+            drawRegularPolygon (x,y) outerDiagonalLength outerSidesCount (head outerAngles)
+    
+    let inscribedRegularPolygonAlignedMultiple (outerSidesCount, outerAngleStart) innerSidesCount iterations = \x y s1 s2 -> do
+            let alignedPolygon innerAngleStart' = inscribedRegularPolygonAligned (outerSidesCount, outerAngleStart) (innerSidesCount, innerAngleStart') x y s1 s2
+            let accumulatePolygons innerAngleStart' accumulator = do
+                    alignedPolygon innerAngleStart'
                     accumulator
+            let initialPolygon = alignedPolygon outerAngleStart
+            let otherPolygons = tail (map (+ outerAngleStart) (polygonAngles iterations))
 
-            foldr accumulatePolygons (initialPolygon) (polygonAngles iterations)
+            foldr accumulatePolygons initialPolygon otherPolygons
  
-    let equilateralTriangleAligned angle = regularPolygonAligned 3 angle
-    
     let trianglePointingDown = \x y rX rY ->
             drawTriangle (x - rX, y - rY) (x, y + rY) (x + rX, y - rY)
     
@@ -104,15 +132,14 @@ drawPolkaDots sceneWidth sceneHeight = do
 
     let elem i j =
             (coordinate i (sceneWidth / (2 * fromInteger circlesX)),
-             coordinate j (sceneHeight / (2 * fromInteger circlesY)),
+             coordinate j (sceneHeight / (2 * fromInteger circlesY)) ,
              radius i j sceneWidth circlesX,
              radius i j sceneHeight circlesY,
              shape i j)
             where
                 coordinate i' radius = radius + radius * 2 * fromInteger i'
                 radius i j dimension circlesPerDimension = dimension / (2 * (fromInteger circlesPerDimension))
-                shape i j
-                    | otherwise = regularPolygonAlignedMultiple 4 pi 200
+                shape i j = inscribedRegularPolygonAlignedMultiple (6, pi) 6 19
 
     let grid = [ elem i j | i <- [0..(circlesX - 1)], j <- [0..(circlesY - 1)] ]
 
